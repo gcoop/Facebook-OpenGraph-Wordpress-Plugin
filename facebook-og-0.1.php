564eeb0713602ecf18f1c01127d02911ff7301dc
Gavin Cooper <gavinsworld@googlemail.com>
2010-07-30 19:22:28 +0100

    Initial commit.

---
+++ facebook-og-0.1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Facebook OpenGraph Protocol Post
+ * Plugin URI: http://wordpress.org/#
+ * Description: Adds the ability to add Facebook OpenGraph meta info to posts.
+ * Author: Gavin Cooper
+ * Version: 0.1
+ * Author URI: http://gavincoop.co.uk/
+ */
+
+?>
